cb67c57780d49d98a74a9235454d3dc4978529c2
Murat Sezgin <msezgin@codeaurora.org>
2016-05-05 14:01:51 -0700

    neighbour: Add a new neigh mac update notifier
    With this new notifier, neighbour subsystem will
    provide a neighbour mac address update notification
    with both the old and new mac adress. This helps the
    other subsystems to do whatever they want with the old
    mac address, if they used it in their own purpose.
    
    Change-Id: I86f6f7107b4c840d05bce167594fdd43d7bb0ff3
    Signed-off-by: Murat Sezgin <msezgin@codeaurora.org>

--- net/core/neighbour.c
+++ net/core/neighbour.c
@@ -1202,7 +1202,19 @@ static void neigh_update_hhs(struct neighbour *neigh)
 	}
 }
 
+ATOMIC_NOTIFIER_HEAD(neigh_mac_update_notifier_list);
 
+void neigh_mac_update_register_notify(struct notifier_block *nb)
+{
+	atomic_notifier_chain_register(&neigh_mac_update_notifier_list, nb);
+}
+EXPORT_SYMBOL_GPL(neigh_mac_update_register_notify);
+
+void neigh_mac_update_unregister_notify(struct notifier_block *nb)
+{
+	atomic_notifier_chain_unregister(&neigh_mac_update_notifier_list, nb);
+}
+EXPORT_SYMBOL_GPL(neigh_mac_update_unregister_notify);
 
 /* Generic update routine.
    -- lladdr is new lladdr or NULL, if it is not supplied.
@@ -1233,6 +1245,7 @@ static int __neigh_update(struct neighbour *neigh, const u8 *lladdr,
 	int notify = 0;
 	struct net_device *dev;
 	int update_isrouter = 0;
+	struct neigh_mac_update nmu;
 
 	trace_neigh_update(neigh, lladdr, new, flags, nlmsg_pid);
 
@@ -1241,6 +1254,8 @@ static int __neigh_update(struct neighbour *neigh, const u8 *lladdr,
 	dev    = neigh->dev;
 	old    = neigh->nud_state;
 	err    = -EPERM;
+
+	memset(&nmu, 0, sizeof(struct neigh_mac_update));
 
 	if (!(flags & NEIGH_UPDATE_F_ADMIN) &&
 	    (old & (NUD_NOARP | NUD_PERMANENT)))
@@ -1276,7 +1291,11 @@ static int __neigh_update(struct neighbour *neigh, const u8 *lladdr,
 		   and a new address is proposed:
 		   - compare new & old
 		   - if they are different, check override flag
+		   - copy old and new addresses for neigh update notification
 		 */
+		memcpy(nmu.old_mac, neigh->ha, dev->addr_len);
+		memcpy(nmu.update_mac, lladdr, dev->addr_len);
+
 		if ((old & NUD_VALID) &&
 		    !memcmp(lladdr, neigh->ha, dev->addr_len))
 			lladdr = neigh->ha;
@@ -1399,8 +1418,11 @@ out:
 	if (((new ^ old) & NUD_PERMANENT) || ext_learn_change)
 		neigh_update_gc_list(neigh);
 
-	if (notify)
+	if (notify) {
 		neigh_update_notify(neigh, nlmsg_pid);
+		atomic_notifier_call_chain(&neigh_mac_update_notifier_list, 0,
+					   (struct neigh_mac_update *)&nmu);
+	}
 
 	trace_neigh_update_done(neigh, err);
 
