9f2b919c206aa823ff6f4040580e3abea4d0c889
Sivaprakash Murugesan <sivaprak@codeaurora.org>
2019-06-15 12:54:16 +0530

    net:qrtr Fix in qrtr_node_release.
    Working case:
    When the remoteproc is restarted, it initiates transfer by
    sending hello packet to APPS and expects the same response from APPS.
    Afterwards the APPS and Q6 would broadcast all of its services to
    each other.This is the initial phase.
    
    However in our case when the remoteproc is restarted, APPS receives
    hello packet from remote. But its not sending hello packet to the
    remote. Its dropped in qrtr layer itself. This message was not sent
    to the remote processor. So when checked, observed that node->ep
    was null which it should never be if qrtr_endpoint_regitser is a
    success, which it was. So it meant something was holding reference
    to the node and qrtr_node_release was not handled properly.
    If node never gets released then it will never be removed from the
    map and when APPS try to send hello packets the old reference
    in the map would be looked which is null in our case.
    
    This has the changes for fixing this scenario.
    
    Signed-off-by: Nikhil Prakash V <nprakash@codeaurora.org>
    (cherry picked from commit 9115334094c064e5ae2cbbbe4e713d5c2983214b)
    Signed-off-by: Sivaprakash Murugesan <sivaprak@codeaurora.org>
    
    Change-Id: I0a6ce40443d44bfde45010afc17a24a296e5ed22

--- net/qrtr/qrtr.c
+++ net/qrtr/qrtr.c
@@ -123,6 +123,7 @@ static DECLARE_RWSEM(qrtr_node_lock);
 /* local port allocation management */
 static DEFINE_IDR(qrtr_ports);
 static DEFINE_MUTEX(qrtr_port_lock);
+static DEFINE_MUTEX(qrtr_node_locking);
 
 /**
  * struct qrtr_node - endpoint node
@@ -323,7 +324,7 @@ static void __qrtr_node_release(struct kref *kref)
 	spin_unlock_irqrestore(&qrtr_nodes_lock, flags);
 
 	list_del(&node->item);
-	up_write(&qrtr_node_lock);
+	mutex_unlock(&qrtr_node_locking);
 
 	/* Free tx flow counters */
 	mutex_lock(&node->qrtr_tx_lock);
@@ -356,7 +357,7 @@ static void qrtr_node_release(struct qrtr_node *node)
 {
 	if (!node)
 		return;
-	kref_put_rwsem_lock(&node->ref, __qrtr_node_release, &qrtr_node_lock);
+	kref_put_mutex(&node->ref, __qrtr_node_release, &qrtr_node_locking);
 }
 
 /**
