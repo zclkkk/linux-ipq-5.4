e198d3910525cd17d676915b1d3e51775cf06b14
Selvam Sathappan Periakaruppan <speriaka@codeaurora.org>
2020-06-05 09:05:08 +0530

    drivers: bus: mhi: Move qrtr_instance_id below SBL Load err checks
    This patch is necessary because without this patch, if suppose
    qrtr_instance_id is not given in DT, then it will be printing
    "MHI did not load SBL" which is not the actual error and it could
    lead to a misinterpretation of failure error msgs.
    
    Signed-off-by: Selvam Sathappan Periakaruppan <speriaka@codeaurora.org>
    Change-Id: I17c9c1bfad9143bf9cc7fad193134992131ddfa1

--- drivers/bus/mhi/core/boot.c
+++ drivers/bus/mhi/core/boot.c
@@ -441,17 +441,6 @@ void mhi_fw_load_handler(struct mhi_controller *mhi_cntrl)
 	ret = mhi_fw_load_sbl(mhi_cntrl, dma_addr, size);
 	mhi_free_coherent(mhi_cntrl, size, buf, dma_addr);
 
-	if (!ret && mhi_cntrl->cntrl_dev->of_node) {
-		ret = of_property_read_u32(mhi_cntrl->cntrl_dev->of_node,
-					   "qrtr_instance_id", &instance);
-		if (!ret) {
-			instance &= QRTR_INSTANCE_MASK;
-			mhi_write_reg_field(mhi_cntrl, mhi_cntrl->bhi,
-					    BHI_ERRDBG2, QRTR_INSTANCE_MASK,
-					    QRTR_INSTANCE_SHIFT, instance);
-		}
-	}
-
 	if (!mhi_cntrl->fbc_download || ret || mhi_cntrl->ee == MHI_EE_EDL)
 		release_firmware(firmware);
 
@@ -463,6 +452,21 @@ void mhi_fw_load_handler(struct mhi_controller *mhi_cntrl)
 
 	if (mhi_cntrl->ee == MHI_EE_EDL)
 		return;
+
+	if (!ret && mhi_cntrl->cntrl_dev->of_node) {
+		ret = of_property_read_u32(mhi_cntrl->cntrl_dev->of_node,
+					   "qrtr_instance_id", &instance);
+		if (!ret) {
+			instance &= QRTR_INSTANCE_MASK;
+			mhi_write_reg_field(mhi_cntrl, mhi_cntrl->bhi,
+					    BHI_ERRDBG2, QRTR_INSTANCE_MASK,
+					    QRTR_INSTANCE_SHIFT, instance);
+		} else {
+			dev_err(dev,
+				"qrtr_instance_id not defined in DT, ret:%d\n",
+				ret);
+		}
+	}
 
 	write_lock_irq(&mhi_cntrl->pm_lock);
 	mhi_cntrl->dev_state = MHI_STATE_RESET;
