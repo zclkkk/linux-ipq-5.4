41bdd3f82947f6f09ef977dfd71d54b83d27c68b
Balaji Prakash J <bjagadee@codeaurora.org>
2020-07-02 14:39:15 +0530

    msm: sps: enclose IPC log creation inside CONFIG_IPC_LOGGING
    When CONFIG_IPC_LOGGING is disabled, IPC log creation
    should be skipped. So, enclosed IPC log creation inside
    IPC_LOGGING config.
    
    Signed-off-by: Rajkumar Ayyasamy <arajkuma@codeaurora.org>
    (cherry picked from commit 263414abc32da8a11d3d68cb27a80ff4c9764804)
    
    Signed-off-by: Balaji Prakash J <bjagadee@codeaurora.org>
    Change-Id: I290e7efb3754fe8a2cf7c3c727a66a4c84adc3d2

--- drivers/platform/msm/sps/sps.c
+++ drivers/platform/msm/sps/sps.c
@@ -2022,7 +2022,9 @@ int sps_register_bam_device(const struct sps_bam_props *bam_props,
 {
 	struct sps_bam *bam = NULL;
 	void __iomem *virt_addr = NULL;
+#ifdef CONFIG_IPC_LOGGING
 	char bam_name[MAX_MSG_LEN];
+#endif
 	u32 manage;
 	int ok;
 	int result;
@@ -2113,6 +2115,7 @@ int sps_register_bam_device(const struct sps_bam_props *bam_props,
 	if (virt_addr != NULL)
 		bam->props.virt_addr = virt_addr;
 
+#ifdef CONFIG_IPC_LOGGING
 	snprintf(bam_name, sizeof(bam_name), "sps_bam_%pa_0",
 					&bam->props.phys_addr);
 	bam->ipc_log0 = ipc_log_context_create(SPS_IPC_LOGPAGES,
@@ -2157,6 +2160,7 @@ int sps_register_bam_device(const struct sps_bam_props *bam_props,
 		bam->ipc_loglevel = bam_props->ipc_loglevel;
 	else
 		bam->ipc_loglevel = SPS_IPC_DEFAULT_LOGLEVEL;
+#endif
 
 	ok = sps_bam_device_init(bam);
 	mutex_unlock(&bam->lock);
@@ -2927,6 +2931,7 @@ static int __init sps_init(void)
 	if (sps == NULL)
 		return -ENOMEM;
 
+#ifdef CONFIG_IPC_LOGGING
 	sps->ipc_log0 = ipc_log_context_create(SPS_IPC_LOGPAGES,
 							"sps_ipc_log0", 0);
 	if (!sps->ipc_log0)
@@ -2947,6 +2952,7 @@ static int __init sps_init(void)
 				SPS_IPC_REG_DUMP_FACTOR, "sps_ipc_log4", 0);
 	if (!sps->ipc_log4)
 		pr_err("Failed to create IPC log4\n");
+#endif
 
 	ret = platform_driver_register(&msm_sps_driver);
 
