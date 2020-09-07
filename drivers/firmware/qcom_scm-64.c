caf-backup/quic_la_kernel_msm

ipq807x: Add scm_call support for external watchdog FIQ handling
Change-Id: Ia07d8231adfe0b7ced3d73e3d4f77672fdd561db
Signed-off-by: Pavithra Palanisamy <pavip@codeaurora.org>

drivers/firmware/qcom_scm-64.c
@@ -521,6 +521,22 @@ int __qcom_scm_regsave(struct device *dev, u32 svc_id, u32 cmd_id,
 	return ret ? : res.a1;
 }
 
+int __qcom_scm_extwdt(struct device *dev, u32 svc_id, u32 cmd_id,
+			unsigned int regaddr, unsigned int val)
+{
+	struct qcom_scm_desc desc = {0};
+	struct arm_smccc_res res;
+	int ret;
+
+	desc.args[0] = (u64)regaddr;
+	desc.args[1] = val;
+	desc.arginfo = SCM_ARGS(2, SCM_RW, SCM_VAL);
+	ret = qcom_scm_call(dev, ARM_SMCCC_OWNER_SIP, SCM_SVC_IO_ACCESS,
+			    QCOM_SCM_EXTWDT_CMD, &desc, &res);
+
+	return ret ? : res.a1;
+}
+
 int __qcom_scm_tcsr(struct device *dev, u32 svc_id, u32 cmd_id,
 			struct qcom_scm_tcsr_req *tcsr_cmd)
 {

zclkkk/linux-ipq-5.4

Signed-off-by: Sivaprakash Murugesan <sivaprak@codeaurora.org>
Change-Id: I41dd73308fb66cedb57633c184db13fff8c60643

drivers/firmware/qcom_scm-64.c
@@ -947,3 +947,19 @@ int __qti_scm_pshold(struct device *dev)
 {
 	return -ENOTSUPP;
 }
+
+int __qcom_scm_extwdt(struct device *dev, u32 svc_id, u32 cmd_id,
+			unsigned int regaddr, unsigned int val)
+{
+	struct qcom_scm_desc desc = {0};
+	struct arm_smccc_res res;
+	int ret;
+
+	desc.args[0] = (u64)regaddr;
+	desc.args[1] = val;
+	desc.arginfo = SCM_ARGS(2, QCOM_SCM_RW, QCOM_SCM_VAL);
+	ret = qcom_scm_call(dev, ARM_SMCCC_OWNER_SIP, QCOM_SCM_SVC_IO,
+			    QCOM_SCM_EXTWDT_CMD, &desc, &res);
+
+	return ret ? : res.a1;
+}