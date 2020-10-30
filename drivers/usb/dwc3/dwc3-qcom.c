48ea84f0e1e84628db82a0095367c658964228ac
Kathiravan T <kathirav@codeaurora.org>
2020-10-30 12:20:51 +0530

    arm64: dts: ipq6018: enable USB2 support
    Signed-off-by: Kathiravan T <kathirav@codeaurora.org>
    Change-Id: I7b0685e94c291bef39388178662fa7421ccba246

--- drivers/usb/dwc3/dwc3-qcom.c
+++ drivers/usb/dwc3/dwc3-qcom.c
@@ -760,6 +760,7 @@ static const struct of_device_id dwc3_qcom_of_match[] = {
 	{ .compatible = "qcom,msm8996-dwc3" },
 	{ .compatible = "qcom,msm8998-dwc3" },
 	{ .compatible = "qcom,sdm845-dwc3" },
+	{ .compatible = "qcom,ipq6018-dwc3" },
 	{ .compatible = "qcom,ipq807x-dwc3" },
 	{ }
 };
