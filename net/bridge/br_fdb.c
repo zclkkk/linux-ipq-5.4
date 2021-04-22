caf-backup/quic_la_kernel_msm

linux: Bridge fdb add or refresh
Create a new function to add bridge fdb entry or refresh
an already existing entry.

Change-Id: Id0c57936bb9cb6e97ba1f15603cd85d50ec56105
Signed-off-by: Subhranil Choudhury <subhrani@codeaurora.org>

net/bridge/br_fdb.c
@@ -1084,6 +1084,54 @@ static int __br_fdb_delete(struct net_bridge_port *p,
 	return err;
 }
 
+/* This function creates a new FDB entry.
+ * The caller can specify the FDB entry type like static,
+ * local or external entry.
+ * This has to be called only for bridge-port netdevs.
+ */
+int br_fdb_add_or_refresh_by_netdev(struct net_device *dev,
+				    const unsigned char *addr, u16 vid,
+				    u16 state)
+{
+	struct net_bridge_fdb_entry *fdb = NULL;
+	struct net_bridge *br = NULL;
+	struct hlist_head *head = NULL;
+	int err = 0;
+	u16 nlh_flags = NLM_F_CREATE;
+	struct net_bridge_port *p = NULL;
+
+	if (!dev) {
+		pr_info("bridge: netdevice is NULL\n");
+		return -EINVAL;
+	}
+
+	rcu_read_lock();
+	p = br_port_get_check_rcu(dev);
+	if (!p) {
+		rcu_read_unlock();
+		pr_info("bridge: %s not a bridge port\n",
+			dev->name);
+		return -EINVAL;
+	}
+
+	br = p->br;
+	head = &br->hash[br_mac_hash(addr, vid)];
+
+	spin_lock_bh(&p->br->hash_lock);
+	fdb = fdb_find(head, addr, vid);
+	if (!fdb) {
+		err = fdb_add_entry(p, addr, state,
+				    nlh_flags, vid);
+	} else {
+		fdb->updated = jiffies;
+	}
+	spin_unlock_bh(&p->br->hash_lock);
+	rcu_read_unlock();
+
+	return err;
+}
+EXPORT_SYMBOL_GPL(br_fdb_add_or_refresh_by_netdev);
+
 /* This function has to be called only for bridge-port netdevs.*/
 /* For bridge netdev br_fdb_delete has to be called.*/
 int br_fdb_delete_by_netdev(struct net_device *dev,

zclkkk/linux-ipq-5.4

Signed-off-by: Pavithra R <pavir@codeaurora.org>

net/bridge/br_fdb.c
@@ -1114,6 +1114,52 @@ static int __br_fdb_delete(struct net_bridge *br,
 	return err;
 }
 
+/* This function creates a new FDB entry.
+ * The caller can specify the FDB entry type like static,
+ * local or external entry.
+ * This has to be called only for bridge-port netdevs.
+ */
+int br_fdb_add_or_refresh_by_netdev(struct net_device *dev,
+				    const unsigned char *addr, u16 vid,
+				    u16 state)
+{
+	struct net_bridge_fdb_entry *fdb = NULL;
+	struct net_bridge *br = NULL;
+	int err = 0;
+	u16 nlh_flags = NLM_F_CREATE;
+	struct net_bridge_port *p = NULL;
+
+	if (!dev) {
+		pr_info("bridge: netdevice is NULL\n");
+		return -EINVAL;
+	}
+
+	rcu_read_lock();
+	p = br_port_get_check_rcu(dev);
+	if (!p) {
+		rcu_read_unlock();
+		pr_info("bridge: %s not a bridge port\n",
+			dev->name);
+		return -EINVAL;
+	}
+
+	br = p->br;
+
+	spin_lock_bh(&br->hash_lock);
+	fdb = br_fdb_find(br, addr, vid);
+	if (!fdb) {
+		err = fdb_add_entry(br, p, addr, state,
+				    nlh_flags, vid, 0);
+	} else {
+		fdb->updated = jiffies;
+	}
+	spin_unlock_bh(&br->hash_lock);
+	rcu_read_unlock();
+
+	return err;
+}
+EXPORT_SYMBOL_GPL(br_fdb_add_or_refresh_by_netdev);
+
 /* This function has to be called only for bridge-port netdevs.*/
 /* For bridge netdev br_fdb_delete has to be called.*/
 int br_fdb_delete_by_netdev(struct net_device *dev,