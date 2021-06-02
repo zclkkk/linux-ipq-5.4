2c5ff3cf58910fa0b87957f37d032babc3d34b9b
Manikanta Mylavarapu <mmanikan@codeaurora.org>
2021-06-02 22:37:48 +0530

    ipq5018: Add apss clock driver support
    Signed-off-by: Manikanta Mylavarapu <mmanikan@codeaurora.org>
    Change-Id: Ied1738f11a6726d4006ccd8ab6dc50656cb13cba

--- drivers/mailbox/qcom-apcs-ipc-mailbox.c
+++ drivers/mailbox/qcom-apcs-ipc-mailbox.c
@@ -29,6 +29,10 @@ struct qcom_apcs_ipc_data {
 	char *clk_name;
 };
 
+static const struct qcom_apcs_ipc_data ipq5018_apcs_data = {
+	.offset = 8, .clk_name = "qcom,apss-ipq5018"
+};
+
 static const struct qcom_apcs_ipc_data ipq6018_apcs_data = {
 	.offset = 8, .clk_name = "qcom,apss-ipq6018-clk"
 };
@@ -145,6 +149,7 @@ static int qcom_apcs_ipc_remove(struct platform_device *pdev)
 
 /* .data is the offset of the ipc register within the global block */
 static const struct of_device_id qcom_apcs_ipc_of_match[] = {
+	{ .compatible = "qcom,ipq5018-apcs-apps-global", .data = &ipq5018_apcs_data },
 	{ .compatible = "qcom,ipq6018-apcs-apps-global", .data = &ipq6018_apcs_data },
 	{ .compatible = "qcom,ipq8074-apcs-apps-global", .data = &ipq8074_apcs_data },
 	{ .compatible = "qcom,msm8916-apcs-kpss-global", .data = &msm8916_apcs_data },
