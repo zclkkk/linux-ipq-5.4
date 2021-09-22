13094725dbaff6184d04378a25358fa43d777c9b
Balamurugan Mahalingam <bmahalin@codeaurora.org>
2021-09-22 19:50:01 +0530

    cnss2: Do not crash when afc buffer cannot be accessed
    Change-Id: I4454c50e29468b3f74a5b2f671a184966b253a42
    
    Signed-off-by: Balamurugan Mahalingam <bmahalin@codeaurora.org>

--- drivers/net/wireless/cnss2/pci.c
+++ drivers/net/wireless/cnss2/pci.c
@@ -2963,8 +2963,7 @@ int cnss_send_buffer_to_afcmem(struct device *dev, char *afcdb, uint32_t len,
 
 	return 0;
 err:
-	CNSS_ASSERT(0);
-	return -ret;
+	return ret;
 }
 EXPORT_SYMBOL(cnss_send_buffer_to_afcmem);
 
@@ -3002,10 +3001,10 @@ int cnss_reset_afcmem(struct device *dev, uint8_t slotid)
 	return 0;
 
 err:
-	CNSS_ASSERT(0);
 	return ret;
 }
 EXPORT_SYMBOL(cnss_reset_afcmem);
+
 int cnss_ahb_alloc_fw_mem(struct cnss_plat_data *plat_priv)
 {
 	struct cnss_fw_mem *fw_mem = plat_priv->fw_mem;
